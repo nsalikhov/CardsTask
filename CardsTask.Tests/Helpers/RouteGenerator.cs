d5485950fa4ec6d9516aae0251dd59dc752d1f1f
Nail Salikhov <nail.salikhov@hotmail.com>
2017-10-09 23:27:17 +0300

    Tests are added.

---
+++ CardsTask.Tests/Helpers/RouteGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using CardsTask.Models;
+
+using Ploeh.AutoFixture;
+
+
+
+namespace CardsTask.Tests.Helpers
+{
+	public static class RouteGenerator
+	{
+		public static IEnumerable<RouteItem> GetRandomRoute()
+		{
+			var routeItems = Enumerable
+				.Range(0, _random.Next(5, 30))
+				.Select(x => _fixture.Create<string>())
+				.ToArray();
+
+			var result = new List<RouteItem>();
+
+			for (int i = 0; i < routeItems.Length - 1; i++)
+			{
+				result.Add(new RouteItem(routeItems[i], routeItems[i + 1]));
+			}
+
+			return result;
+		}
+
+		public static IEnumerable<RouteItem> ShuffleRoute(IEnumerable<RouteItem> route)
+		{
+			return route.OrderBy(x => _random.Next());
+		}
+
+		private static readonly Random _random = new Random();
+		private static readonly Fixture _fixture = new Fixture();
+	}
+}
---
+++ CardsTask.Tests/Services/RouteBuilderTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Linq;
+
+using CardsTask.Exceptions;
+using CardsTask.Models;
+using CardsTask.Services;
+using CardsTask.Tests.Helpers;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+namespace CardsTask.Tests.Services
+{
+	[TestClass]
+	public class RouteBuilderTests
+	{
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			_target = new RouteBuilder();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(CardsTaskException))]
+		public void BuildRoute_EmptyRouteTest()
+		{
+			_target.BuildRoute(new RouteItem[0]);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(CardsTaskException))]
+		public void BuildRoute_DuplicateDepartureTest()
+		{
+			_target.BuildRoute(
+				new[]
+				{
+					new RouteItem("Moscow", "London"),
+					new RouteItem("Moscow", "Beijing")
+				});
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(CardsTaskException))]
+		public void BuildRoute_DuplicateDestinationTest()
+		{
+			_target.BuildRoute(
+				new[]
+				{
+					new RouteItem("Moscow", "London"),
+					new RouteItem("Beijing", "London")
+				});
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(CardsTaskException))]
+		public void BuildRoute_CycleTest()
+		{
+			_target.BuildRoute(
+				new[]
+				{
+					new RouteItem("Moscow", "London"),
+					new RouteItem("London", "Beijing"),
+					new RouteItem("Beijing", "Moscow")
+				});
+		}
+
+		[TestMethod]
+		public void BuildRoute_Test()
+		{
+			var route = RouteGenerator.GetRandomRoute().ToArray();
+			var unorderedRoute = RouteGenerator.ShuffleRoute(route);
+
+			var result = _target.BuildRoute(unorderedRoute);
+
+			result.ShouldAllBeEquivalentTo(route, o => o.WithStrictOrdering());
+		}
+
+		private RouteBuilder _target;
+	}
+}
